e47f73269704e66e4a9f9c4f4522326b307aa1ef
ENJ-digitalhub <ekwerenoble2@gmail.com>
2025-11-15 01:01:29 +0100

    statupPage

--- PoultryFarmManagementSystem.java
+++ PoultryFarmManagementSystem.java
@@ -1,21 +1,31 @@
+//for accepting input
 import java.util.Scanner;
+//for date and time
+import java.util.Date;
 
 public class PoultryFarmManagementSystem{
+	//Object declared static so in can be accessable for all methods
 	static Scanner read = new Scanner(System.in);
 	public static void clearScreen() {
+		/*\033 = ESC character
+			[2J = clear entire screen
+			[H = move cursor to top-left*/
         System.out.print("\033[H\033[2J");
+		//flush() ensures the output happens immediately
         System.out.flush();
     }
 	public static void exit(){
 		clearScreen();
-		System.out.println("Closing program . . . . . .");
+		System.out.println("Closing program . . . . .");
 	}
 	public static boolean confirm(double x){
 		clearScreen();
 		System.out.print("Confirm \""+x+" \"(Y,N)?");
 		boolean isConfirm=false;
 		char confirm=read.next().charAt(0);
+		//change character to lowercase.
 		confirm=Character.toLowerCase(confirm);
+		//confirming input
 		if (confirm=='y'){
 			clearScreen();
 			System.out.println("Saving. . . . .");
@@ -34,4 +44,32 @@ public class PoultryFarmManagementSystem{
 	public static void home(){}
 	public static void recordEdit(){}
 	public static void eggGraph(){}
-	public static void main(String[] args){}
+	public static void register(){}
+	public static void login(){}
+	public static void startupPage(){
+		System.out.print("1. Login\n2. Register\n3. Exit\nOption>");
+		int option=read.nextInt();
+		switch (option){
+			case 1:
+				clearScreen();
+				login();
+				break;
+			case 2:
+				clearScreen();
+				register();
+				break;
+			case 3:
+				clearScreen();
+				exit();
+				break;
+			default:
+				clearScreen();
+				System.out.println("Invalid selection");
+				startupPage();
+				break;
+		}
+	}
+	public static void main(String[] args){
+		startupPage();
+	}
+}	
