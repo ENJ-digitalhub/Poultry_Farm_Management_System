c785d4ae01ba7a5d76ffabebf9ad19aacd1e7e42
ENJ-digitalhub <ekwerenoble2@gmail.com>
2025-12-16 19:59:19 +0100

    clearscreen edited for lower version cmd

--- PF/SystemUtils.java
+++ PF/SystemUtils.java
@@ -22,9 +22,19 @@ public class SystemUtils{
         return " ".repeat((int)padding)+s+" ".repeat((int)padding);
     }
     public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    try {
+        if (System.getProperty("os.name").contains("Windows")) {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } 
+		else {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
+    } 
+	catch (Exception e) {
+        for (int i = 0; i < 50; i++) System.out.println();
     }
+}
     public static void exit(){
         clearScreen();
         System.out.println("Closing program . . . . .");
